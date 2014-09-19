90b151a4c35d25574ec5143b5b6e2c8577ed7b4f
Shin'ya Ueoka <qeexee@i-beam.org>
2014-09-19 14:39:17 +0900

    Renamed files

--- file_list.rb
+++ file_list.rb
@@ -1,11 +1,13 @@
 def editor_files
+  Dir.glob(File.expand_path('../src/core/**/*.coffee',  __FILE__)) +
   Dir.glob(File.expand_path('../src/editor/**/*.coffee',  __FILE__))
 end
 
 def viewer_files
   Dir.glob(File.expand_path('../src/main.coffee',  __FILE__)) +
   Dir.glob(File.expand_path('../src/core/**/*.coffee',  __FILE__)) +
-  Dir.glob(File.expand_path('../src/plugins/**/*.coffee',  __FILE__))
+  Dir.glob(File.expand_path('../src/viewer/**/*.coffee',  __FILE__)) +
+  Dir.glob(File.expand_path('../src/viewer-plugins/**/*.coffee',  __FILE__))
 end
 
 def spec_files
