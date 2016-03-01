3a76abaec78e09a095b79994039241cae8303066
Shin'ya Ueoka <ueokande@i-beam.org>
2016-03-01 17:53:44 +0900

    Port location.coffee to .js

---
+++ src/plugins/location.js
@@ -0,0 +1,28 @@
+Libretto.Location = class extends Libretto.Plugin {
+  initialize() {
+    window.addEventListener('hashchange', (e) => {
+      Libretto.Location.prototype.applyPage.call(this);
+    })
+
+    let viewer = Libretto.Viewer.viewer()
+    viewer.addCurrentPageChangedListener(() => {
+      let index = viewer.getCurrentIndex()
+      Libretto.Location.prototype.setHash.call(this, index);
+    });
+
+    Libretto.Location.prototype.applyPage.call(this);
+  }
+
+  setHash(index) {
+    window.location.hash = index + 1
+  }
+
+  applyPage() {
+    let num = +(window.location.hash.split('#')[1])
+    if (isNaN(num)) { num = 1; }
+    let viewer = Libretto.Viewer.viewer()
+    viewer.skipToPage(num - 1)
+  }
+}
+
+new Libretto.Location
