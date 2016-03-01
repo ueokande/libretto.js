eeae8807b94ec374c8b0d72d1b5083ec438ee17c
Shin'ya Ueoka <ueokande@i-beam.org>
2016-03-01 16:31:35 +0900

    Port page_transition.coffee to .js

---
+++ src/page_transition.js
@@ -0,0 +1,67 @@
+Libretto.PageTransition = class {
+  constructor(prevIndex, nextIndex, nextPage, cssId) {
+    this.prevIndex = prevIndex;
+    this.nextIndex = nextIndex;
+    this.nextPage = nextPage;
+    this.pageAnimeCss = Libretto.Css.findOrCreate(cssId)
+  }
+
+  finalize() {
+    this.pageAnimeCss.finalize()
+  }
+
+  switchPage(animationEnable) {
+    if (animationEnable) {
+      this.animatePage();
+    } else {
+      this.noAnimatePage();
+    }
+  }
+
+  noAnimatePage() {
+    this.execAnime();
+  }
+
+  animatePage() {
+    let effectName = this.nextPage.animationEffect()
+    let duration = this.nextPage.animationDuration()
+    let options = this.nextPage.animationOptions()
+
+    var effect;
+    if (effectName == null) {
+      effect = null
+    } else {
+      effect = Libretto.loadPageEffect(effectName);
+    }
+    if (effect === null) {
+      console.warn(`No such page effect : ${effectName}`)
+    }
+
+    if (effect == null) {
+      this.execAnime();
+    } else {
+      this.execAnime(effect(), duration, options)
+    }
+  }
+
+  execAnime(effect, duration, options) {
+    this.pageAnimeCss.clearRules();
+    let prevStyle, nextStyle;
+    if (this.prevIndex !== null) {
+      prevStyle = this.pageAnimeCss.addRule(`section:nth-of-type(${this.prevIndex+1})`)
+      prevStyle.visibility = 'visible'
+      prevStyle.zIndex = 0
+    }
+    nextStyle = this.pageAnimeCss.addRule(`section:nth-of-type(${this.nextIndex+1})`)
+    nextStyle.visibility = 'visible'
+    nextStyle.zIndex = 1
+
+    if (effect === undefined) { return }
+
+    if (effect.hasOwnProperty('before')) {
+      effect.before(prevStyle, nextStyle, duration, options)
+    }
+    setTimeout(() => { effect.exec(prevStyle, nextStyle, duration, options)},
+              50);  // 50ms is hack to run on Firefox
+  }
+}
