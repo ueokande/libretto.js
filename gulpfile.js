c7d13709876ba18f87333857d87632a8684eb891
Shin'ya Ueoka <ueokande@i-beam.org>
2016-03-02 09:04:03 +0900

    Setup ESLint

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var sass = require('gulp-sass');
+var eslint = require('gulp-eslint');
 
 gulp.task('build', function() {
   var concat = require('gulp-concat');
@@ -31,4 +32,30 @@ gulp.task('gh-pages', function() {
   .pipe(sass().on('error', sass.logError))
   .pipe(gulp.dest('build'));
   return [copyRaw, buildCoffee, buildCss];
+});
+
+gulp.task('build', function() {
+  var concat = require('gulp-concat');
+  var buildSrc = gulp.src(['src/index.js', 'src/**/*.js'])
+  .pipe(babel({ presets: ["es2015"] }))
+  .pipe(concat('libretto.js'))
+  .pipe(gulp.dest('build'));
+
+  var buildSpec = gulp.src('spec/**/*_spec.js')
+  .pipe(babel({ presets: ["es2015"] }))
+  .pipe(concat('spec.js'))
+  .pipe(gulp.dest('build'));
+
+  var buildCss = gulp.src('css/**/*.{sass,scss}')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('build'));
+
+  return [buildSrc, buildSpec, buildCss];
+});
+
+gulp.task('lint', function () {
+  return gulp.src(['src/**/*.js'])
+  .pipe(eslint())
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError());
 });
