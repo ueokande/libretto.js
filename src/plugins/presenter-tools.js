6aa1b7b3267927306589b890be8cbc72efc0e8bd
Shin'ya Ueoka <ueokande@i-beam.org>
2016-04-24 17:49:33 +0900

    Toggle window

--- src/plugins/presenter-tools.js
+++ src/plugins/presenter-tools.js
@@ -1,11 +1,16 @@
 class PresenterTool extends Libretto.Plugin {
   initialize() {
     window.addEventListener('keypress', (e) => {
-      if (e.charCode == 'p'.charCodeAt(0) &&
-          typeof this.toolWindow === 'undefined') {
-        this.toolWindow = window.open('about:blank', 'Presenter Tools',
-                                 'width=400');
-        Libretto.IO.registerEvents(this.toolWindow);
+      if (e.charCode == 'p'.charCodeAt(0)) {
+        if (typeof this.toolWindow === 'undefined') {
+          this.toolWindow = window.open('about:blank', 'Presenter Tools',
+                                   'width=400');
+          this.toolWindow.addEventListener('keypress', (e) => {
+            e.target.ownerDocument.defaultView.close();
+            delete this.toolWindow;
+          });
+          Libretto.IO.registerEvents(this.toolWindow);
+        }
       }
     });
   }
